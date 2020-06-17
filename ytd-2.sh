f9d8725b63d294ef1c70c071a60e244104cb42b0
Daniel Nogueira de Oliveira <daniel.n.oliveira@outlook.com>
2020-06-17 02:49:29 -0300

    update

--- ytd-2.sh
+++ ytd-2.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sudo youtube-dl  -ci --playlist-items $2 --extract-audio --audio-format mp3 --audio-quality 6 -o '%(title)s.%(ext)s' $1
+youtube-dl  -ci --playlist-items $2 --extract-audio --audio-format mp3 --audio-quality 6 -o '%(title)s.%(ext)s' $1
+mv *.mp3 ../storage/music
