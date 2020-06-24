86e19ec3da0b94cbe4dfdd797ec0c6caae69dd49
Daniel Nogueira de Oliveira <daniel.n.oliveira@outlook.com>
2020-06-24 19:24:19 -0300

    update

--- ytd-2.sh
+++ ytd-2.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 youtube-dl  -ci --playlist-items $2 --extract-audio --audio-format mp3 --audio-quality 6 -o '%(title)s.%(ext)s' $1
-mv *.mp3 ../storage/music
+
+mv *.mp3 ../../Music/musicDontEdited
